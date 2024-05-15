d37fc652b81f8b908bd41b52894573768396c976
RichardGarita <richard.garita@ucr.ac.cr>
2024-05-14 23:37:17 -0600

    Implemented the add URL form structure
    - Installed React Hook Form for form management
    - Added validation for the full URL input using a regex pattern
    - Included a section to display the generated shortcode

--- src/components/header/components/addUrlModal.jsx
+++ src/components/header/components/addUrlModal.jsx
@@ -1,8 +1,25 @@
 import { useState } from 'react';
 import Modal from '../../../utils/modal';
+import { useForm } from 'react-hook-form';
 
 export default function AddUrl(){
     const [showModal, setShowModal] = useState(false);
+    const [shortCode, setShortCode] = useState('');
+    const {register, handleSubmit, formState: { errors, isValid },  } = useForm({mode: "all"});
+
+    const onSubmit = (data) => {
+        console.log(data);
+        setShortCode(data.full_url);
+    }
+
+    const urlRegex = new RegExp(
+        /(?:(?:(https?|ftp):)?\/\/)/.source       // protocol
+        + /(?:([^:\n\r]+):([^@\n\r]+)@)?/.source  // user:pass
+        + /(?:(?:www.)?([^/\n\r]+))/.source       // domain
+        + /(\/[^?\n\r]+)?/.source                 // request
+        + /(\?[^#\n\r]*)?/.source                 // query
+        + /(#?[^\n\r]*)?/.source                  // anchor
+    );
 
     return (
         <>
@@ -12,7 +29,31 @@ export default function AddUrl(){
                 title={'Generate your short code!'}
                 content={
                     <>
-                        <h3>Working</h3>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className='form-group text-start mb-3'>
+                                <label htmlFor='full_url' className='me-auto'><strong>Full Url:</strong></label>
+                                <input type='url' className='form-control' id='full_url' name='full_url' 
+                                placeholder='https://www.example.com' {...register('full_url', {
+                                    required: {value: true, message: 'The full url is required'},
+                                    pattern: {
+                                        value: urlRegex,
+                                        message: "The Url must have a valid format"
+                                    },
+                                })}/>
+                            </div>
+                            {errors.full_url && <small className='text-start text-danger d-block'>{errors.full_url.message}</small>}
+
+                            <div className='text-end'>
+                                <button type='submit' disabled={!isValid} className='btn btn-primary ms-auto'>Generate</button>
+                            </div>
+
+                            <section className={`mt-1 ${shortCode ? '' : 'd-none'}`}>
+                                <p>Here is your short code. Make sure you save it!</p>
+                                <div className='shortcode-box'>
+                                    <p><strong>{shortCode}</strong></p>
+                                </div>
+                            </section>
+                        </form>
                     </>
                 }
             />
